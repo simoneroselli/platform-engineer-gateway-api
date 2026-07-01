887bf4354a71c3d041079e3fbdfe458301070b95
Simone Roselli <zmo@germanomacbook.fritz.box>
2026-07-01 22:55:02 +0200

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+#k3d cluster create gateway-api --agents 2 -p "8080:30080@agent:0" -p "80:30080@agent:0"
+#kubectl create ns argocd
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
