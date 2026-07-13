8e235b68178ed0296a018fbdb892947ccd081f71
Simone Roselli <zmo@germanomacbook.fritz.box>
2026-07-13 17:59:08 +0200

    Update Wordsmith operator to correct resource name and enhance RBAC permissions

--- platform-workloads/operators/wordsmith-ops/operator.py
+++ platform-workloads/operators/wordsmith-ops/operator.py
@@ -1,10 +1,10 @@
 import kopf
 import kubernetes
 
-@kopf.on.create('padre.maronno', 'v1', 'myapps')
+@kopf.on.create('padre.maronno', 'v1', 'wordsmithapps')
 def create_fn(spec, name, namespace, logger, **kwargs):
     # This function triggers when you create a new "MyApp" CR
-    logger.info(f"A new MyApp {name} was created with spec: {spec}")
+    logger.info(f"A new wordsmithapps {name} was created with spec: {spec}")
 
     # You can add logic here to create Deployments, Services, etc.
     # Kopf makes this feel like writing simple Python functions!
